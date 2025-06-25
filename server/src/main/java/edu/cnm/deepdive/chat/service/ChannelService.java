c378b288d12eec3f2e1a4bfec3f4c8057e5eb429
nick-bennett <nbennett10@cnm.edu>
2025-06-25 14:20:21 -0600

    Implemented business logic operations.

---
+++ server/src/main/java/edu/cnm/deepdive/chat/service/ChannelService.java
@@ -0,0 +1,31 @@
+package edu.cnm.deepdive.chat.service;
+
+import edu.cnm.deepdive.chat.model.entity.Channel;
+import edu.cnm.deepdive.chat.service.dao.ChannelRepository;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChannelService implements AbstractChannelService {
+
+  private final ChannelRepository repository;
+
+  @Autowired
+  ChannelService(ChannelRepository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public Iterable<Channel> getAll() {
+    return repository.getAllByOrderByTitleAsc();
+  }
+
+  @Override
+  public Channel get(UUID key) {
+    return repository
+        .findByExternalKey(key)
+        .orElseThrow();
+  }
+
+}
