2fc0c0af104853ac2dc7cb8a494046e394aa1020
tanickell <tnickell1@cnm.edu>
2025-06-26 11:03:10 -0600

    Started Google Sign-In on client.

---
+++ app/src/main/java/edu/cnm/deepdive/chat/controller/MainActivity.java
@@ -0,0 +1,51 @@
+package edu.cnm.deepdive.chat.controller;
+
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import dagger.hilt.android.AndroidEntryPoint;
+import edu.cnm.deepdive.chat.R;
+import edu.cnm.deepdive.chat.databinding.ActivityMainBinding;
+
+@AndroidEntryPoint
+public class MainActivity extends AppCompatActivity {
+
+  private static final String TAG = MainActivity.class.getSimpleName();
+
+  private ActivityMainBinding binding;
+  private AppBarConfiguration appBarConfig;
+  private NavController navController;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setupUI();
+    setupNavigation();
+  }
+
+  @Override
+  public boolean onSupportNavigateUp() {
+    return NavigationUI.navigateUp(navController, appBarConfig);
+  }
+
+  private void setupUI() {
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    EdgeToEdge.enable(this);
+    setContentView(binding.getRoot());
+  }
+
+  private void setupNavigation() {
+    setSupportActionBar(binding.toolbar);
+    appBarConfig = new AppBarConfiguration.Builder(
+        R.id.pre_login_fragment, R.id.login_fragment, R.id.main_fragment)
+        .build();
+    NavHostFragment host = binding.navHostFragment.getFragment();
+    navController = host.getNavController(); // reference to the piece of machinery that will swap in one fragment for another
+    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
+  }
+
+}
---
+++ app/src/main/java/edu/cnm/deepdive/chat/viewmodel/LoginViewModel.java
@@ -0,0 +1,97 @@
+package edu.cnm.deepdive.chat.viewmodel;
+
+import android.content.Intent;
+import android.util.Log;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import dagger.hilt.android.lifecycle.HiltViewModel;
+import edu.cnm.deepdive.chat.service.GoogleSignInService;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import javax.inject.Inject;
+
+/** @noinspection deprecation*/
+@HiltViewModel
+public class LoginViewModel extends ViewModel implements DefaultLifecycleObserver {
+
+  private static final String TAG = LoginViewModel.class.getSimpleName();
+
+  private final GoogleSignInService service;
+  private final MutableLiveData<GoogleSignInAccount> account;
+  private final MutableLiveData<Throwable> refreshThrowable;
+  private final MutableLiveData<Throwable> signInThrowable;
+  private final CompositeDisposable pending;
+
+  @Inject
+  LoginViewModel(GoogleSignInService service) {
+    this.service = service;
+    account = new MutableLiveData<>();
+    refreshThrowable = new MutableLiveData<>();
+    signInThrowable = new MutableLiveData<>();
+    pending = new CompositeDisposable();
+  }
+
+  public LiveData<Throwable> getSignInThrowable() {
+    return signInThrowable;
+  }
+
+  public LiveData<Throwable> getRefreshThrowable() {
+    return refreshThrowable;
+  }
+
+  public LiveData<GoogleSignInAccount> getAccount() {
+    return account;
+  }
+
+  public void refresh() {
+    refreshThrowable.setValue(null);
+    signInThrowable.setValue(null);
+    service
+        .refresh()
+        .subscribe(
+            account::postValue,
+            (throwable) -> postThrowable(throwable, refreshThrowable),
+            pending // subscribe method will not only turn the power on, it will take the claim check and put it into that pending bucket
+        );
+  }
+
+  public void startSignIn(@NonNull ActivityResultLauncher<Intent> launcher) {
+    refreshThrowable.setValue(null);
+    signInThrowable.setValue(null);
+    service.startSignIn(launcher);
+  }
+
+  public void completeSignIn(@NonNull ActivityResult result) { // invoked from the UI (could be when the view is created, or could be invoked from sign-in button from UI) --> UI thread
+    refreshThrowable.setValue(null);
+    signInThrowable.setValue(null);
+    service
+        .completeSignIn(result)
+        .subscribe(
+            account::postValue,          // since I don't know it's on the UI thread, we must use postValue
+            (throwable) -> postThrowable(throwable, signInThrowable),
+            pending
+        );
+  }
+
+  public void signOut() {
+    refreshThrowable.setValue(null);
+    signInThrowable.setValue(null);
+    service
+        .signOut()
+        .doFinally(() -> account.postValue(null)) // doesn't happen in order: doFinally doesn't execute until the stream is done
+        .doOnError((throwable) -> postThrowable(throwable, signInThrowable))
+        .subscribe();
+  }
+
+  private void postThrowable(
+      @NonNull Throwable throwable, @NonNull MutableLiveData<Throwable> target) {
+    Log.e(TAG, throwable.getMessage(), throwable); // log, then put into live data
+    target.postValue(throwable);
+  }
+
+}
