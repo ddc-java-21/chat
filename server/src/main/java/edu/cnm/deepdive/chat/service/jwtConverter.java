1ed05d0ef2fa308244bc8472bf0769625c7882d7
tmomonet <thollins@cnm.edu>
2025-06-26 16:36:41 -0600

    Resolving Filename changes

--- server/src/main/java/edu/cnm/deepdive/chat/service/jwtConverter.java
+++ server/src/main/java/edu/cnm/deepdive/chat/service/jwtConverter.java
@@ -4,6 +4,7 @@ import edu.cnm.deepdive.chat.model.entity.User;
 import java.util.Collection;
 import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -11,25 +12,26 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 @Service
-public class jwtConverter implements Converter<Jwt, UsernamePasswordAuthenticationToken> {
+@Profile("service")
+public class JwtConverter implements Converter<Jwt, UsernamePasswordAuthenticationToken> {
 
   private final AbstractUserService userService;
 
   @Autowired
-  jwtConverter(AbstractUserService userService) {
+  JwtConverter(AbstractUserService userService) {
     this.userService = userService;
   }
-
 
   @Override
   public UsernamePasswordAuthenticationToken convert(Jwt source) {
     Collection<SimpleGrantedAuthority> grants =
         Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
-    User user =  new User();
+    User user = new User();
     user.setDisplayName(source.getClaimAsString("name"));
     user.setAvatar(source.getClaimAsURL("picture"));
     user.setOauthKey(source.getSubject());
     user = userService.getOrAddUser(source.getSubject(), user);
     return new UsernamePasswordAuthenticationToken(user, source.getTokenValue(), grants);
   }
+
 }
