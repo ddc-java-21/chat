684a4b22b389ebf3fad40dc220d3e4b37655bdc8
tmomonet <thollins@cnm.edu>
2025-06-27 11:39:17 -0600

    Added Exception handler for NoSuchElementException

---
+++ server/src/main/java/edu/cnm/deepdive/chat/controller/ExceptionMapper.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive.chat.controller;
+
+import java.util.NoSuchElementException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionMapper {
+
+  @ExceptionHandler(NoSuchElementException.class)
+  @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Specified user, channel, or message not found")
+  public void notFound() {
+
+  }
+}
