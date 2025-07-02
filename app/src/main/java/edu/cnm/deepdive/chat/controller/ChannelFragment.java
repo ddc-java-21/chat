8d3eff8690ad45a8d1721ad8a40e215573150cf1
tanickell <tnickell1@cnm.edu>
2025-07-02 10:54:16 -0600

    Adding outgoing message flow.

--- app/src/main/java/edu/cnm/deepdive/chat/controller/ChannelFragment.java
+++ app/src/main/java/edu/cnm/deepdive/chat/controller/ChannelFragment.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.chat.controller;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,15 +9,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import dagger.hilt.android.AndroidEntryPoint;
 import edu.cnm.deepdive.chat.databinding.FragmentChannelBinding;
 import edu.cnm.deepdive.chat.model.dto.Channel;
+import edu.cnm.deepdive.chat.model.dto.Message;
+import edu.cnm.deepdive.chat.viewmodel.ChatViewModel;
 
 @AndroidEntryPoint
 public class ChannelFragment extends Fragment {
 
+  private static final String TAG = ChannelFragment.class.getSimpleName();
+
   private FragmentChannelBinding binding;
   private Channel channel;
+  private ChatViewModel viewModel;
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,13 +40,25 @@ public class ChannelFragment extends Fragment {
     ((AppCompatActivity) requireActivity())
         .getSupportActionBar()
         .setTitle(channel.getTitle());
+    binding.send.setOnClickListener((v) -> {
+      Message message = new Message();
+      message.setText(binding.message.getText().toString().strip());
+      viewModel.sendMessage(message);
+    });
     return binding.getRoot();
   }
 
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    // TODO: 7/1/25 Attach to viewmodel(s) and observe livedata.
+    viewModel = new ViewModelProvider(requireActivity()).get(ChatViewModel.class);
+    viewModel.setSelectedChannel(channel);
+    viewModel
+        .getMessages()
+        .observe(getViewLifecycleOwner(), (messages) -> {
+          Log.d(TAG, messages.toString());
+          // TODO: 7/2/25 Pass new messages to a recyclerview adapter.
+        });
   }
 
   @Override
