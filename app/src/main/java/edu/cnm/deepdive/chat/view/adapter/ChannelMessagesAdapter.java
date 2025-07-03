cd66e94c4d951d4ca10071de337ced5f8d83ee57
Dorian <dobisike@cnm.edu>
2025-07-03 09:46:17 -0600

    Working on message list display.

---
+++ app/src/main/java/edu/cnm/deepdive/chat/view/adapter/ChannelMessagesAdapter.java
@@ -0,0 +1,30 @@
+package edu.cnm.deepdive.chat.view.adapter;
+
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+
+public class ChannelMessagesAdapter extends Adapter<ViewHolder> {
+
+  @NonNull
+  @Override
+  public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    return null;
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+  }
+
+  @Override
+  public int getItemCount() {
+    return 0;
+  }
+
+  private class MessageViewHolder extends ViewHolder {
+
+  }
+
+}
