35a3368c5a9986d8f1bf4cead2c1c77d95cc795a
nick-bennett <nbennett10@cnm.edu>
2025-07-29 09:17:43 -0600

    Add automatic idle-time quoting.

---
+++ server/src/main/java/edu/cnm/deepdive/chat/model/dto/ZenQuote.java
@@ -0,0 +1,29 @@
+package edu.cnm.deepdive.chat.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ZenQuote {
+
+  @JsonProperty("q")
+  private String quote;
+
+  @JsonProperty("a")
+  private String author;
+
+  public String getQuote() {
+    return quote;
+  }
+
+  public void setQuote(String quote) {
+    this.quote = quote;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+}
